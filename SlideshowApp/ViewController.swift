297d6718b3d2409ec7d3a9fa5085722420934ae9
masanori.kobori <livephotobank@yahoo.co.jp>
2019-08-30 15:32:07 +0900

    Initial Commit

---
+++ SlideshowApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SlideshowApp
+//
+//  Created by Kobori on 2019/08/30.
+//  Copyright © 2019 Masanori Kobori. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
