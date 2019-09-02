eeec303840d15eca4a8021cc14de6cb73ffc39ee
masanori.kobori <livephotobank@yahoo.co.jp>
2019-09-02 16:36:02 +0900

    9/2 16:35

--- SlideshowApp/ViewController.swift
+++ SlideshowApp/ViewController.swift
@@ -30,16 +30,15 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     
-        
+        //写真の呼び出しについての関数？
         func imageView() {
             let imageView = images[0]    // 表示している画像の番号から名前を取り出し
-            // let image = UIImage(named: name)    // 画像を読み込み？？　■■■エラーになる
             UIImageView.image = imageView // Image Viewに読み込んだ画像をセット
         }
         
     }
     
-    // 更新用関数になるのかな？(更新するために作った関数
+    // 更新用関数(更新するために作った関数
     @objc func onTimer(_ timer: Timer) {
         if imageIndex == 2 {
             imageIndex = 0
@@ -68,25 +67,21 @@ class ViewController: UIViewController {
     @IBAction func startStop(_ sender: Any) {
         // 動作中のタイマーを1つに保つために、 timer が存在しない場合だけタイマーを生成して動作させる
         if self.timer == nil {         // タイマーを設定
-            self.timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(onTimer(_:)), userInfo: nil, repeats: true)      //Timer.scheduledTimerでタイマーを作成、始動していて、その後の()に入っている記述は引数
-            //nextImage.isEnabled = false     // 再生・停止ボタンタップ時に進むボタン・戻るボタンのタップ非表示 ■■■エラーになる
-            //backImage.isEnabled = false    ■■■エラーになる
-            //startStop.setTitle("停止", for: .normal)   // ボタンの名前を停止とする  ■■■エラーになる
+            self.timer = Timer.scheduledTimer(timeInterval: 2.0, target: self, selector: #selector(onTimer(_:)), userInfo: nil, repeats: true)      //Timer.scheduledTimerでタイマーを作成、始動していて、その後の()に入っている記述は引数
+            //nextImage.isEnabled = false   // 再生・停止ボタンタップ時に進むボタン・戻るボタンのタップ非表示にしたい！■■■エラーになる
+            //backImage.isEnabled = false   // 再生・停止ボタンタップ時に進むボタン・戻るボタンのタップ非表示にしたい！■■■エラーになる
+            //startStop.setTitle("停止", for: .normal)    // ボタンの名前を停止とする■■■エラーになる
         } else if self.timer != nil {
-            // タイマーを停止
-            self.timer.invalidate()
-            // nil にして再び再生(nil の時にタイマー生成)
-            self.timer = nil
-            // 再生・停止ボタンタップ時に進むボタン・戻るボタンのタップ非表示
-            //nextImage.isEnabled = true    ■■■エラーになる
-            //backImage.isEnabled = true    ■■■エラーになる
-            // ボタンの名前を再生とする
-            //startStop.setTitle("再生", for: .normal)  ■■■エラーになる
+            self.timer.invalidate()     // タイマーを停止
+            self.timer = nil    // nil にして再び再生(nil の時にタイマー生成)
+            //nextImage.isEnabled = true       // 再生・停止ボタンタップ時に進むボタン・戻るボタンのタップ非表示にしたい！■■■エラーになる
+            //backImage.isEnabled = true       // 再生・停止ボタンタップ時に進むボタン・戻るボタンのタップ非表示にしたい！■■■エラーになる
+            //startStop.setTitle("再生", for: .normal)        // ボタンの名前を再生とする■■■エラーになる
         }
     }
     // startTimer() の timer == nil で判断するために、 timer = nil としておく
     //　A_ここまで
-    // timeInterval: 1.0    1.0 秒ごとに更新されるタイマー
+    // timeInterval: 2.0    2.0秒ごとに更新されるタイマー
     // target: self,    ターゲットは自分(クラスのViewController.swift )を指す
     // selector: #selector(updateTimer(_:)      updateTimerを実行するように#selector(updateTimer(_:)と引数に指定している
     // userInfo: nil,   タイマーとして渡したい値が設定するけど、今回は不要なのでnil
